1cd8f9b8135ce6d15fcbf23086c04dc77a6d9de2
HelenEdwards <hgedwards87@gmail.com>
2022-01-06 08:52:58 -0800

    fixing background img

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.addEventListener(
     function () {
         const canvas = document.getElementById('game-canvas');
         new Game(canvas);
+        // window.style.backgroundImage = "url(yggdrasil.png)"
     }
 )
 
